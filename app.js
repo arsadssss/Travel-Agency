50fc0fcdddffd7dafd799d2067f116ee030d96e0
arsadssss <arsadsagir@gmail.com>
2024-09-17 19:44:11 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+$(function () { 
+    $('[data-toggle="tooltip"]').tooltip({trigger: 'manual'}).tooltip('show');
+  });  
+    $(".progress-bar").each(function(){
+      each_bar_width = $(this).attr('aria-valuenow');
+      $(this).width(each_bar_width + '%');
+    });
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Travel Agency Landing Page
+
+<h4>Tech Used</h4>
+- HTML
+- CSS
+- BootStrap
+- JQuery
